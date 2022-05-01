53b2d2fcb2a4b8ffbc4bed51ded0fb4b163b24f1
KevinUfkes <kevinufkes@gmail.com>
2022-04-30 18:35:56 -0700

    home page

---
+++ src/components/Home/Home.js
@@ -0,0 +1,12 @@
+import './Home.css'
+
+function Home() {
+    return (
+      <div className="App">
+        <h1>Kevin Ufkes</h1>
+        <a href=''>Projects</a>
+      </div>
+    );
+}
+
+export default Home;
