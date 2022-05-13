a0e2d53a913584e22565e18d52aa81a533a5ee08
KevinUfkes <kevinufkes@gmail.com>
2022-05-13 11:39:32 -0700

    fix

--- src/index.js
+++ src/index.js
@@ -4,8 +4,8 @@ import './index.css';
 import App from './components/App/App';
 import reportWebVitals from './reportWebVitals';
 
-import Amplify from 'aws-amplify';
-import config from './aws-exports';
+// import Amplify from 'aws-amplify';
+// import config from './aws-exports';
 // Amplify.configure(config);
 
 
