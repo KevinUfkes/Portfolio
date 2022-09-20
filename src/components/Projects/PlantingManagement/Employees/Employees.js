190ff4cc52f3a3b387774bee814553a3baf4d890
KevinUfkes <kevinufkes@gmail.com>
2022-09-20 15:09:22 -0700

    cosmectic enhancements

--- src/components/Projects/PlantingManagement/Employees/Employees.js
+++ src/components/Projects/PlantingManagement/Employees/Employees.js
@@ -1,5 +1,6 @@
 
-import 'bootstrap/dist/css/bootstrap.css';
+// import 'bootstrap/dist/css/bootstrap.css';
+import './Employees.css'
 import React, { useEffect, useState } from 'react'
 import { getEmployees, createEmployee, deleteEmployee } from '../MongoRoutes/EmployeeRoutes.js'
 import { getRoles } from '../MongoRoutes/RoleRoutes.js';
@@ -51,66 +52,62 @@ function Employees() {
 
     return (
         <>
-
-        <h1>Employees</h1>
-        {/* <h2>{JSON.stringify(employees)}</h2> */}
-          <Button href="/projects/planting_management">Planting Management</Button>   
-            <div>Employees</div>
-            <div className="App">
-              <div className="row">
-                <div className='col col-3'>
-                  <h3>Add Employee</h3>
-                  <Form onSubmit={e => {handleSubmit(e)}}>
+          <h1>Employees</h1>
+          <div className="App">
+            <div className="row">
+              <div className='col col-3'>
+                <h3>Add Employee</h3>
+                <Form onSubmit={e => {handleSubmit(e)}}>
+                  <Form.Group>
+                    <Form.Label>First Name</Form.Label>
+                    <Form.Control type='text' placeholder='Enter first name' value={newFirstName} onChange={e => setNewFirstName(e.target.value)}/>
+                  </Form.Group>
+                  <Form.Group>
+                    <Form.Label>Last Name</Form.Label>
+                    <Form.Control type='text' placeholder='Enter last name' value={newLastName} onChange={e => setNewLastName(e.target.value)}/>
+                  </Form.Group>
+                  <Form.Group>
+                    <Form.Label>Email</Form.Label>
+                    <Form.Control type='email' placeholder='Enter email' value={newEmail} onChange={e => setNewEmail(e.target.value)}/>
+                  </Form.Group>
+                  {roles.map((role) => 
                     <Form.Group>
-                      <Form.Label>First Name</Form.Label>
-                      <Form.Control type='text' placeholder='Enter first name' value={newFirstName} onChange={e => setNewFirstName(e.target.value)}/>
+                      <Form.Check type="checkbox" label={role.name} name={role._id} onChange={handleChangeRolesCheckbox}/>
                     </Form.Group>
-                    <Form.Group>
-                      <Form.Label>Last Name</Form.Label>
-                      <Form.Control type='text' placeholder='Enter last name' value={newLastName} onChange={e => setNewLastName(e.target.value)}/>
-                    </Form.Group>
-                    <Form.Group>
-                      <Form.Label>Email</Form.Label>
-                      <Form.Control type='email' placeholder='Enter email' value={newEmail} onChange={e => setNewEmail(e.target.value)}/>
-                    </Form.Group>
-                    {roles.map((role) => 
-                      <Form.Group>
-                        <Form.Check type="checkbox" label={role.name} name={role._id} onChange={handleChangeRolesCheckbox}/>
-                      </Form.Group>
-                    )}
-                    <Button variant='primary' type='submit'>Add Employee</Button>
-                  </Form> 
-                </div>
-                <div className='col col-9'> 
-                  <Table>
-                    <thead>
-                      <tr>
-                        <th>First Name</th>
-                        <th>Last Name</th>
-                        <th>Email</th>
-                        <th>View Details</th>
-                        <th>Update</th>
-                        <th>Delete</th>
-                      </tr>
-                    </thead>
-                    <tbody>
-                      {
-                        employees.map((employee) => 
-                          <tr>
-                            <td>{employee.first_name}</td>
-                            <td>{employee.last_name}</td>
-                            <td>{employee.email}</td>
-                            <td><Button>Detials</Button></td>
-                            <td><Link to="/projects/planting_management/update_planter" state={{employee_state: JSON.stringify(employee)}}>Update</Link></td>
-                            <td><Button value={employee._id} onClick={e => deleteEmployee(employee._id)}>Delete</Button></td>
-                          </tr>
-                        )
-                      }
-                    </tbody>
-                  </Table>
-                </div>
+                  )}
+                  <Button variant='primary' type='submit'>Add Employee</Button>
+                </Form> 
+              </div>
+              <div className='col col-9'> 
+                <Table striped bordered hover>
+                  <thead>
+                    <tr>
+                      <th>First Name</th>
+                      <th>Last Name</th>
+                      <th>Email</th>
+                      <th>View Details</th>
+                      <th>Update</th>
+                      <th>Delete</th>
+                    </tr>
+                  </thead>
+                  <tbody>
+                    {
+                      employees.map((employee) => 
+                        <tr>
+                          <td>{employee.first_name}</td>
+                          <td>{employee.last_name}</td>
+                          <td>{employee.email}</td>
+                          <td><Button>Detials</Button></td>
+                          <td><Link to="/projects/planting_management/update_planter" state={{employee_state: JSON.stringify(employee)}}>Update</Link></td>
+                          <td><Button value={employee._id} onClick={e => deleteEmployee(employee._id)} variant="danger">Delete</Button></td>
+                        </tr>
+                      )
+                    }
+                  </tbody>
+                </Table>
               </div>
             </div>
+          </div>
         </>
     );
 }
--- src/components/Projects/PlantingManagement/PlantingManagement.js
+++ src/components/Projects/PlantingManagement/PlantingManagement.js
@@ -1,17 +1,27 @@
 import './PlantingManagement.css'
 import 'bootstrap/dist/css/bootstrap.css';
+import marthas_crew from './../../../images/marthas_crew.jpg'
 import React from 'react'
-import Button from 'react-bootstrap/Button'
+import Navigation from '../../Navigation/Navigation.js';
 
 
 function PlantingManagement() {
     return (
         <>
             <div className="App"> 
+                <Navigation
+                    bg = "dark"
+                    expand = 'lg'
+                    title = {["Planting Management", "/projects/planting_management"]}
+                    links = {[
+                        ["About", "/projects/planting_management/about"],
+                        ["Employees", "/projects/planting_management/employees"],
+                        ["Crews", "/projects/planting_management/crews"], 
+                        ["Create Crews", "/projects/planting_management/crews/create"],
+                      ]}
+                />
                 <h1>Planting Management</h1>  
-
-                <Button href="/projects/planting_management/employees">Employees</Button>
-                <Button href="/projects/planting_management/crews">Crews</Button>
+                <img src={marthas_crew} alt="Martha's planting crew Artisan 2018"/>
                 
             </div>
         </>
