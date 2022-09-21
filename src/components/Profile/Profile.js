fc79b93c6b02b2c8ea481c843e960835508af520
KevinUfkes <kevinufkes@gmail.com>
2022-09-21 12:13:00 -0700

    title favicon

--- src/components/Profile/Profile.js
+++ src/components/Profile/Profile.js
@@ -1,6 +1,7 @@
 import React from 'react';
 import resume_kevin_ufkes from './../../images/resume_kevin_ufkes.png'
 import Card from 'react-bootstrap/Card';
+import Table from 'react-bootstrap/Table';
 
 function Profile() {
     return(
@@ -11,7 +12,23 @@ function Profile() {
                         <div className='col'>
                             <Card className='main_card'>
                                 <Card.Title className='main_card_title'><h1>Profile</h1></Card.Title>
+                                <h4>About Me</h4>
+                                <p>
+                                    <ul className='main_ul'>
+                                        <li>
+                                            <strong>Name: </strong>Kevin Ufkes
+                                        </li>
+                                        <li>
+                                            <strong>Hometown: </strong> Peterborough, ON, Canada 
+                                        </li>
+                                        <li>
+                                            <strong>Current Residence: </strong> Nelson, BC, Canada
+                                        </li>
+                                    </ul>
+                                    
+                                </p>
                                 {/* <Card.Img className='main_card_img' src={resume_kevin_ufkes}></Card.Img> */}
+                                <h4>Resumé</h4>
                                 <img className='main_card_img' src={resume_kevin_ufkes}/>
                             </Card>
                         </div>
