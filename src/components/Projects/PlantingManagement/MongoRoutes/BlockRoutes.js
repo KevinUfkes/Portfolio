7752e257afc0b4777fd346d14400781cbb72f3e3
KevinUfkes <kevinufkes@gmail.com>
2022-09-27 20:01:16 -0700

    create block (incomplete)

---
+++ src/components/Projects/PlantingManagement/MongoRoutes/BlockRoutes.js
@@ -0,0 +1,26 @@
+import axios from 'axios';
+
+export async function getBlocks(){
+    const blocksResponse = await fetch(`https://us-west-2.aws.data.mongodb-api.com/app/application-0-wadcn/endpoint/pm/blocks`);
+    
+        if (!blocksResponse.ok) {
+          const message = `An error occurred: ${blocksResponse.statusText}`;
+          window.alert(message);
+          return;
+        }
+        const blocks = await blocksResponse.json();
+        return blocks;
+}
+
+export async function createBlock(block_code, area, density, price, contract) {
+    axios.post(`https://us-west-2.aws.data.mongodb-api.com/app/application-0-wadcn/endpoint/pm/block/create`, {
+        block_code: block_code,
+        area: area,
+        density: density,
+        price: price,
+        contract: contract
+    }).then(response => {
+      console.log(response)
+      window.location.reload()
+    })
+  }
--- src/components/Projects/PlantingManagement/Pages/Blocks/Blocks.js
+++ src/components/Projects/PlantingManagement/Pages/Blocks/Blocks.js
@@ -1,11 +1,157 @@
-import React from 'react';
+import React, { useEffect, useState } from 'react';
+import { getBlocks, createBlock } from '../../MongoRoutes/BlockRoutes.js';
+import { getContracts } from '../../MongoRoutes/ContractRoutes.js';
+import PMNavigation from '../../Components/PMNavigation/PMNavigation.js';
+import Form from 'react-bootstrap/Form';
+import Card from 'react-bootstrap/Card';
+import Table from 'react-bootstrap/Table';
+import Button from 'react-bootstrap/Button';
+import { Link } from 'react-router-dom';
 
 function Blocks() {
 
+    const [blocks, setBlocks] = useState([]);
+    const [contracts, setContracts] = useState([]);
+    const [blockCode, setBlockCode] = useState();
+    const [area, setArea] = useState(0.00);
+    const [density, setDensity] = useState(0);
+    const [price, setPrice] = useState(0.00);
+    const [contract, setContract] = useState();
+
+    const handleSubmit = (e) => {
+        console.log("BlockCode: " + JSON.stringify(blockCode))
+        console.log("Area: " + JSON.stringify(area))
+        console.log("density: " + JSON.strigify(density))
+        console.log("price: " + JSON.stringify(price))
+        console.log("contract: " + JSON.stringify(contract))
+        createBlock(blockCode, area, density, price, contract)
+        // setBlockCode('')
+        // setArea('')
+        // setDensity('')
+        // setPrice('')
+        // setContract('')
+        e.preventDefault();
+    }
+
+    function deleteContract(){
+        return
+    }
+
+    useEffect(() => {
+
+        async function loadBlocks(){
+            setBlocks(await getBlocks())
+        }
+
+        async function loadContracts(){
+            setContracts(await getContracts())
+        }
+
+        loadBlocks();
+        loadContracts();
+
+        return;
+
+    }, [blocks.length]);
+
     return(
         <>
-            <h1>Blocks</h1>
-        
+            <div className='App pm'>
+                <PMNavigation/>
+                <div className='container'>
+                <Card className='pm_card pm_card_base'>
+                    <Card.Title><h1>Blocks</h1></Card.Title>
+                    <Card.Body>
+                    <div className='container'>
+                        <div className='row'>
+                        <div className='col col-3'>
+                            <Card className='pm_card'>
+                            <Card.Title className="pm_card_title">Add Block</Card.Title>
+                                <Card.Body>
+                                <Form onSubmit={e => {handleSubmit(e)}} className="pm_form">
+                                    <Form.Group>
+                                        <Form.Label>Block Code</Form.Label>
+                                        <Form.Control type='text' placeholder='Enter block code' value={blockCode} onChange={e => setBlockCode(e.target.value)}/>
+                                    </Form.Group>
+                                    <Form.Group>
+                                        <Form.Label>Area</Form.Label>
+                                        <Form.Control type='number' placeholder='Enter area' value={area} onChange={e => setArea(e.target.value)}/>
+                                    </Form.Group>
+                                    <Form.Group>
+                                        <Form.Label>Density</Form.Label>
+                                        <Form.Control type='number' placeholder='Enter density' value={density} onChange={e => setDensity(e.target.value)}/>
+                                    </Form.Group>
+                                    <Form.Group>
+                                        <Form.Label>Price</Form.Label>
+                                        <Form.Control type='number' placeholder='Enter price' value={price} onChange={e => setPrice(e.target.value)}/>
+                                    </Form.Group>
+                                    <Form.Group>
+                                        <Form.Label>Contract Code</Form.Label>
+                                        <Form.Select onChange={e => setContract(e.target.value)}>
+                                            <option disabled selected value> Select a Contract Code</option>
+                                            {
+                                                contracts.map((contract) => {
+                                                    return (
+                                                        <option value={contract._id}>{contract.company_name} - {contract.contract_code}</option>
+                                                    )
+                                                })
+                                            }
+                                        </Form.Select>
+                                    </Form.Group>
+                                    <Button variant='success' type='submit'>Add Block</Button>
+                                </Form> 
+                            </Card.Body>
+                            </Card>
+                        </div>
+                        <div className='col col-9'>
+                            <Card className='pm_card'>
+                            <Card.Body>
+                                <Table  striped bordered hover className='pm_table'>
+                                <thead>
+                                    <tr>
+                                        <th>Block Code</th>
+                                        <th>Area (Hectres</th>
+                                        <th>Density</th>
+                                        <th>Price</th>
+                                        <th>Contract Code</th>
+                                        <th>View Details</th>
+                                        <th>Update</th>
+                                        <th>Delete</th>
+                                    </tr>
+                                </thead>
+                                <tbody>
+                                    {
+                                        blocks.map((block) => 
+                                            <tr>
+                                                <td>{block.block_code}</td>
+                                                <td>{block.area}</td>
+                                                <td>{block.density}</td>
+                                                <td>{block.price}</td>
+                                                {contracts.map((contract) => {
+                                                    if(block.contract == contract._id){
+                                                        return(
+                                                            <td>{contract.contract_code}</td>
+                                                        )
+                                                    }
+                                                })}
+                                                {/* <td>{blocks.contract_code}</td> */}
+                                                <td><Button>Details</Button></td>
+                                                <td><Link to="/projects/planting_management/contracts/update_contract" state={{block_state: JSON.stringify(block)}}><Button variant="warning">Update</Button></Link></td>
+                                                <td><Button value={block._id} onClick={e => deleteContract(block._id)} variant="danger">Delete</Button></td>
+                                            </tr>
+                                        )
+                                    }
+                                </tbody>
+                                </Table>
+                            </Card.Body>
+                            </Card>
+                        </div>
+                        </div>
+                    </div> 
+                    </Card.Body>
+                </Card>
+                </div>
+            </div>     
         </>
     )
 }
