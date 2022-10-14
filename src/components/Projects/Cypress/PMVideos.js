937a526e4d788b8a1386ddcdf6d64a7d94e72d10
KevinUfkes <kevinufkes@gmail.com>
2022-10-14 14:22:54 -0700

    links to cypress portfolio

--- src/components/Projects/Cypress/PMVideos.js
+++ src/components/Projects/Cypress/PMVideos.js
@@ -6,7 +6,9 @@ function PMVideos(){
     return(
         <>
             <h1>Cypress Planting Management Videos</h1>
-            <video src={pm_00002_add_planter} width="1000" controls="controls"/>
+            Visit full Cypress repository <a href="https://github.com/KevinUfkes/PortfolioCypress" target="_blank">here</a>.<br/>
+            <video src={pm_00002_add_planter} width="1000" controls="controls"/><br/>
+            <a href="https://github.com/KevinUfkes/PortfolioCypress/blob/main/cypress/e2e/Planting_Management/Employees/PM-00002.cy.js" target="_blank">See Cypress Code</a>
         </>
     )
 }
