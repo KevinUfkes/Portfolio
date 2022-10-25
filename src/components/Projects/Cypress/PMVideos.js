78a296ee5818a983052165ce12fa29d3a2e5a71f
KevinUfkes <kevinufkes@gmail.com>
2022-10-25 15:07:32 -0700

    youtube embed

--- src/components/Projects/Cypress/PMVideos.js
+++ src/components/Projects/Cypress/PMVideos.js
@@ -1,5 +1,5 @@
 import React from 'react';
-import pm_00002_add_planter from './PMTestVideos/pm_00002_add_planter.mkv'
+// import pm_00002_add_planter from 'https://youtu.be/u494dRzowC0'
 
 function PMVideos(){
 
@@ -7,7 +7,15 @@ function PMVideos(){
         <>
             <h1>Cypress Planting Management Videos</h1>
             Visit full Cypress repository <a href="https://github.com/KevinUfkes/PortfolioCypress" target="_blank">here</a>.<br/>
-            <video src={pm_00002_add_planter} width="1000" controls="controls"/><br/>
+            <iframe 
+                src='https://youtube.com/embed/u494dRzowC0' 
+                title="Embedded YouTube"
+                width="900"
+                height="600"
+                allowFullScreen
+            />
+            <br></br>
+            {/* <video src={pm_00002_add_planter} width="1000" controls="controls"/><br/> */}
             <a href="https://github.com/KevinUfkes/PortfolioCypress/blob/main/cypress/e2e/Planting_Management/Employees/PM-00002.cy.js" target="_blank">See Cypress Code</a>
         </>
     )
